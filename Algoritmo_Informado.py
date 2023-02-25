848fac33c78481fc9bc640069194c3287fd23f38
Juyo84 <juliomeme@hotmail.com>
2023-02-24 21:40:04 -0600

    Listo

--- Algoritmo_Informado.py
+++ Algoritmo_Informado.py
@@ -102,19 +102,22 @@ def algoritmo(valor_h, datos):
 
     #TERMINA EL ALGORIMO
     return (list([capacidad_Actual, beneficio, nodos_Factibles]))
-
+start_time = time.time()
 #REALIZA n ITERACIONES
-for iteraciones in range(0,1,1):
+for iteraciones in range(0,10000,1):
+    
     for filename in os.listdir(folder + 'low-dimensional\\'):
         if filename.endswith(".csv"):
             nombre = filename.split('_')
             datos = asignar_Instancias(filename)    #  PASAMOS LAS INSTANCIAS A UNA VARIABLE
-            start_time = time.time()
+            
             resultado = algoritmo(beneficio_peso(datos), datos)
-            runtime = time.time() - start_time
+            
             #SE IMPRIME LOS RESULTADOS
             #print("-------------------------------------------------------------------------")
             print("Capacidad: " + str("{:.4f}".format(resultado[0])) + "      Beneficio: " + str("{:.4f}".format(resultado[1])) + "       Nodos: "
-                  + str(resultado[2]) + "        Runtime: " + str(runtime) + "\n")
+                  + str(resultado[2]))
             
             datos = []
+runtime = time.time() - start_time
+print("Runtime: " + str("{:.15f}".format(runtime)) + "\n")
